ae463d03288038b43fd8d36088ae43b1d82b088a
Andrés García <jose.andres.gm29@gmail.com>
2018-02-14 16:34:45 -0600

    Add eslintignore to the test folder

---
+++ Test/User.test.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const sinon = require('sinon');
+
+const assert = chai.assert;
+const expect = chai.expect;
+
+describe('User', () => {
+  it('Add a new user', () => {
+    
+  });
+});
