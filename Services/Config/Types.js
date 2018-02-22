dc159d33070913f80523edeae97597e1e6d5109f
Andrés García <jose.andres.gm29@gmail.com>
2018-02-21 23:14:22 -0600

    Refactor Types, doing disccount better

--- Services/Config/Types.js
+++ Services/Config/Types.js
@@ -1,20 +1,8 @@
 module.exports = {
-  twoPerOne: (object) => {
-    let totally = 0;
-    if (object.qty % 2 === 0) {
-      totally += parseInt((object.qty / 2), 10) * object.price;
-      return totally;
-    }
-    totally += (parseInt((object.qty / 2), 10) * object.price) + object.price;
-    return totally;
-  },
-  morethreeProducts: (object) => {
-    let totally = 0;
-    if (object.qty >= 3) {
-      totally += object.qty * (object.price - 1);
-      return totally;
-    }
-    totally += object.qty * object.price;
-    return totally;
-  },
+  twoPerOne: object => (Math.ceil(object.qty / 2) * object.price),
+  morethreeProducts: object => (
+    object.qty >= 3 ?
+      object.qty * (object.price - 1) :
+      object.qty * object.price
+  ),
 };
